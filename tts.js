513370bf095c4ffd2d37d6408eb651eb703ff4eb
luoguang60 <luoguang60@gmail.com>
2026-03-04 13:57:29 +0800

    Update tts.js

--- tts.js
+++ tts.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -40,4 +40,4 @@ export default async function handler(req, res) {
       error: error.message 
     });
   }
-}
+};
